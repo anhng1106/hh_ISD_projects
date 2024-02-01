d3bcc107266ac39469f9e6c1bec37468a3393e8e
Anh Nguyen <123630454+anhng1106@users.noreply.github.com>
2024-02-01 08:46:10 +0200

    Add files via upload

---
+++ Chapter_1/WebGLEarthAPI.js
@@ -0,0 +1,11 @@
+function goHome() {
+  // todo
+  earth.setZoom(13);
+  earth.setView([10.826217263602285, 106.62244430558847]);
+}
+
+function showCoords() {
+  // todo
+  alert(earth.getCenter());
+  earth.setView([41.893292620836945, 12.483070431973514]);
+}
